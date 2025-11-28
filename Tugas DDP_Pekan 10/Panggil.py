3e1aef7718c2d1fb27cdb03f5f16077c2a4b1be3
Kayla Azzahra <kylazhra14@gmail.com>
2025-11-28 18:40:45 +0700

    Tugas Pekan 10

---
+++ Tugas DDP_Pekan 10/Panggil.py
@@ -0,0 +1,15 @@
+import Bangun_Datar, Bangun_Ruang
+
+print("====Luas Bangun Datar====")
+print(f"Luas Persegi = {Bangun_Datar.luas_persegi (5)}")
+print(f"Luas Segitiga = {Bangun_Datar.luas_segitiga (5, 5)}")
+print(f"Luas Lingkaran = {Bangun_Datar.luas_lingkaran (5)}")
+print(f"Luas Ketupat = {Bangun_Datar.luas_ketupat (5, 5)}")
+print(f"Luas Jajar Genjang = {Bangun_Datar.luas_jajar_genjang (5, 5)}")
+
+print("====Luas Bangun Ruang====")
+print(f"Luas Kubus = {Bangun_Ruang.luas_kubus (5)}")
+print(f"Luas Balok = {Bangun_Ruang.luas_balok (5, 5, 5)}")
+print(f"Luas Bola = {Bangun_Ruang.luas_bola (5)}")
+print(f"Luas Tabung = {Bangun_Ruang.luas_tabung (5, 5)}")
+print(f"Luas Kerucut = {Bangun_Ruang.luas_kerucut (5, 5)}")
